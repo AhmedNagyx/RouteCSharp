c7d28f9806225e57cb2d5869ef85eb5160fc9a68
AhmedNagyx <deadevilgamer32@gmail.com>
2025-03-02 00:29:39 +0200

    1

---
+++ RouteC#/Course.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouteC_
+{
+    public class Course
+    {
+        //annotation
+        [Key]
+        public int Id { get; set; }
+        public int Duration { get; set; }
+        [Required,MaxLength(50)]
+        public string Name { get; set; }
+        [MaxLength(200)]
+        public string Description { get; set; }
+        public int Top_Id { get; set; }
+    }
+}
---
+++ RouteC#/Instructor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouteC_
+{
+    public class Instructor
+    {
+
+        //fluent
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Bonus { get; set; }
+        public decimal Salary { get; set; }
+        public string Address { get; set; }
+        public int HourRate { get; set; }
+        public int Dept_Id { get; set; }
+    }
+}
